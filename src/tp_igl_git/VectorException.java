8fd428b2cd5de59e47a3bf679b63e405cdfac2c1
Chemsou Harouit <Chemsou Harouit@HarouitChemsou>
2017-10-21 09:19:24 +0100

    test class created

--- src/tp_igl_git/VectorException.java
+++ src/tp_igl_git/VectorException.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tp_igl_git;
+package TP_IGL_Git;
 
 /**
  *
@@ -13,6 +13,6 @@ public class VectorException extends Exception {
     
     static public String getMassage()
     {
-        return  " Error 0x1 : Vecteurs de taille differentes" ;
+        return  " Error 0x1Vecteurs de taille differentes" ;
     }
 }
