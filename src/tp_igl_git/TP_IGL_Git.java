c1d787414b9fc8982bffd170420c06c899ea09c1
Chemsou Harouit <Chemsou Harouit@HarouitChemsou>
2017-10-21 09:02:52 +0100

    Creating a tab in the main

--- src/tp_igl_git/TP_IGL_Git.java
+++ src/tp_igl_git/TP_IGL_Git.java
@@ -15,7 +15,7 @@ public class TP_IGL_Git {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[] tab= new int[VectorHelper.TAILLETAB];
     }
     
 }
