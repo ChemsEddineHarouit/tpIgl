073f531658cc0c543b2795bf0408054bdf9edcbf
Chemsou Harouit <Chemsou Harouit@HarouitChemsou>
2017-10-21 08:57:59 +0100

    Exceeption created

---
+++ src/tp_igl_git/VectorException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP_IGL_Git;
+
+/**
+ *
+ * @author chemsou
+ */
+public class VectorException extends Exception {
+    
+    static public String getMassage()
+    {
+        return  "Vecteurs de taille differentes" ;
+    }
+}
